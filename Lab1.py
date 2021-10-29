6a4b3702a4e04df52509b7ad8ea8bd3b648b4c5c
DaniilTS <d.tsukrov@mail.ru>
2021-10-29 23:53:32 +0300

    add discret system creation

--- Lab1.py
+++ Lab1.py
@@ -1,10 +1,38 @@
 import numpy as np
 
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def random():
+    return np.random.rand()
+
+
+def build_dsv(P, A, B):
+    q = np.sum(P, axis=1)  # 0-сумма по столбцам, 1- по строкам
+
+    n, m = P.shape
+    
+    l = []
+    for i in range(1, n + 1):
+        l.append(sum(q[0:i]))
+
+    k = np.searchsorted(l, random())
+    x1 = A[k]
+
+    rs = np.zeros(m)
+    for i in range(1, m + 1):
+        rs[i - 1] = rs[i - 2] + P[k, i - 1]
+
+    s = np.searchsorted(rs, random() * rs[-1])
+    x2 = B[s]
+    return x1, x2
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    P = np.array([[0.1, 0.4],
+                  [0.2, 0.1],
+                  [0.1, 0.1]])
+    A = np.array([1, 2, 4])
+    B = np.array([1, 3])
 
+    print('Матрица вероятности P:\n', P)
+
+    dsv = build_dsv(P, A, B)
